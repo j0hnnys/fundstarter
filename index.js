04ec106160b506d95b4890d0d750d1e857bf29dc
Johnny Nguyen <johnnytn.mail@gmail.com>
2016-03-16 21:35:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 // Part 1
 var fs = require('fs');
 
@@ -14,3 +15,19 @@ var rfs = fs.readFileSync('index.html', 'utf-8');
 console.log(rfs);
 
 return true;
+*/
+var express = require('express')
+var app = express()
+
+/* serves all the static files*/
+app.set('port', (process.env.PORT || 8080))
+app.use(express.static(__dirname + '/public'))
+
+/*serves main page*/
+app.get('/', function(request, response) {
+response.sendfile('index.html') 
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
